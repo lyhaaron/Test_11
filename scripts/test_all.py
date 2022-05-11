781da514d080ef98fa70bfa7eaf07476eeceb4f7
lyhaaron <406536691@qq.com>
2022-05-12 01:22:10 +0800

    Initial commit

---
+++ scripts/test_all.py
@@ -0,0 +1,12 @@
+class Test_allure:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_al1(self):
+        assert 0
+
+    def test_al2(self):
+        assert 1
